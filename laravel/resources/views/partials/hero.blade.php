f11dd7f4061e9e450d61e96cb9149af83be37f3f
svenons <gotvonisvens@gmail.com>
2024-11-01 12:20:44 +0100

    bootstrap js back in action

--- laravel/resources/views/partials/hero.blade.php
+++ laravel/resources/views/partials/hero.blade.php
@@ -27,3 +27,6 @@
     </div>
     @livewire('courses')
 </section>
+
+<!-- Add Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
