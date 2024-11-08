ae41bff0070c083490a1503810d627d9d0458ce0
svenons <gotvonisvens@gmail.com>
2024-11-08 12:44:19 +0100

    remove unnecessary shit

--- laravel/resources/views/homepage.blade.php
+++ laravel/resources/views/homepage.blade.php
@@ -1,17 +1,5 @@
 <x-app-layout>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MoldyCampus - Discover The Taste of the University Mould</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        @livewireStyles
-    </head>
-
-    <body>
-
         <!-- hero section -->
         @include('partials.homepage.hero')
 
@@ -22,7 +10,5 @@
         @include('partials.homepage.professors')
 
         @livewireScripts
-    </body>
-
-    </html>
+        
 </x-app-layout>
