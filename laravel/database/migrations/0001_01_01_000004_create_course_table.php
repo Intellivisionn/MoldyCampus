fdb4cc8f5ac906a878d98ef5c7ebaabefeaa468d
svenons <gotvonisvens@gmail.com>
2024-11-10 18:22:17 +0100

    fix

--- laravel/database/migrations/0001_01_01_000004_create_course_table.php
+++ laravel/database/migrations/0001_01_01_000004_create_course_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCoursesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->uuid('id')->primary();
@@ -44,13 +42,11 @@ class CreateCoursesTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('course_student_assistant');
         Schema::dropIfExists('course_professor');
         Schema::dropIfExists('courses');
     }
-}
+};
--- laravel/database/migrations/0001_01_01_000005_create_course_ratings_table.php
+++ laravel/database/migrations/0001_01_01_000005_create_course_ratings_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCourseRatingsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('course_ratings', function (Blueprint $table) {
             $table->uuid('id')->primary();
@@ -26,11 +24,9 @@ class CreateCourseRatingsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('course_ratings');
     }
-}
+};
