b140d490211336f126da8e49d2de91476b100a0c
LP271 <linardspakalns@gmail.com>
2024-11-30 12:09:41 +0100

    Small change admin panel

--- laravel/resources/views/livewire/pages/admin/admin.blade.php
+++ laravel/resources/views/livewire/pages/admin/admin.blade.php
@@ -38,12 +38,7 @@
     <div class="mt-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <!-- Flex container for side-by-side layout -->
         <div class="flex flex-wrap gap-6 justify-between">
-            <div class="flex-1 p-4 bg-white shadow sm:p-8 sm:rounded-lg max-w-sm">
-                <div class="flex items-center justify-between">
-                    <h4>Total Users</h4>
-                    <span class="text-2xl font-bold">{{ $userCount }}</span>
-                </div>
-            </div>
+            
             <div class="flex-1 p-4 bg-white shadow sm:p-8 sm:rounded-lg max-w-sm">
                 <div class="flex items-center justify-between">
                     <h4>Total Courses</h4>
@@ -54,6 +49,12 @@
                 <div class="flex items-center justify-between">
                     <h4>Total Lecturers</h4>
                     <span class="text-2xl font-bold">{{ $professorCount }}</span>
+                </div>
+            </div>
+            <div class="flex-1 p-4 bg-white shadow sm:p-8 sm:rounded-lg max-w-sm">
+                <div class="flex items-center justify-between">
+                    <h4>Total Users</h4>
+                    <span class="text-2xl font-bold">{{ $userCount }}</span>
                 </div>
             </div>
         </div>
