94e20834e451845bca3d520c48d4fbab79ff10c1
svenons <gotvonisvens@gmail.com>
2024-12-06 15:51:24 +0100

    Add APIController and Sanctum integration for authentication and course, professor post&get requests

---
+++ laravel/app/Http/Controllers/APIController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Course;
+use App\Models\Professor;
+use App\Models\CourseRating;
+
+class APIController
+{
+    public function getCourses(request $request)
+    {
+        $allCourses = [];
+        $course = Course::all();
+        foreach ($course as $c) {
+
+            $c->professor = Professor::find($c->professor_id);
+            $c->ratings = CourseRating::find($c->id);
+            $allCourses[] = [
+                'course' => $c,
+                'professor' => $c->professor,
+                'ratings' => $c->ratings
+            ];
+        }
+        return response()->json($allCourses);
+    }
+    public function getProfessors(request $request)
+    {
+        $professor = Professor::all();
+        foreach ($professor as $p) {
+            $p->ratings = CourseRating::find($p->professor_id);
+        }
+        return response()->json($professor);
+    }
+    public function getIndividualCourse(request $request)
+    {
+        $request->validate([
+            'id' => 'required|integer'
+        ]);
+        $course = Course::find($id);
+        $courseRatings = CourseRating::where('course_id', $id)->get();
+        return response()->json(['course' => $course, 'ratings' => $courseRatings]);
+    }
+    public function getIndividualProfessor(request $request)
+    {
+        $request->validate([
+            'id' => 'required|integer'
+        ]);
+        $professor = Professor::find($id);
+        $professorRatings = ProfessorRating::where('professor_id', $id)->get();
+        return response()->json(['professor' => $professor, 'ratings' => $professorRatings]);
+    }
+    public function updateUserProfile(request $request)
+    {
+        $request->validate([
+            'userId' => 'required|integer'
+        ]);
+        
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthAPIController;
+use App\Http\Controllers\APIController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('login', [AuthAPIController::class, 'login']);
 Route::post('register', [AuthAPIController::class, 'register']);
 Route::post('forgot-password', [AuthAPIController::class, 'forgotPassword']);
 Route::post('reset-password', [AuthAPIController::class, 'resetPassword']);
 Route::post('logout', [AuthAPIController::class, 'logout']);
+
+//Courses Routes
+Route::post('courses', [APIController::class, 'getCourses']);
+Route::post('course', [APIController::class, 'getIndividualCourse']);
+Route::post('professors', [APIController::class, 'getProfessors']);
+Route::post('professor', [APIController::class, 'getIndividualProfessor']);
