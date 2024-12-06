629b731104a1c84cd36a62be332dc964ef479769
LP271 <linardspakalns@gmail.com>
2024-12-06 23:53:59 +0100

    Add is_admin column to users table with default value

--- laravel/database/migrations/0001_01_01_000004_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000004_create_users_table.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->is_admin();
-        });
+            $table->boolean('is_admin')->default(false);
+            });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
