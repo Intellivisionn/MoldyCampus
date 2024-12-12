88f427afed56d18636cb96233df3a60d0a176fd7
FilipFekete <fifek23@student.sdu.dk>
2024-12-12 17:39:20 +0100

    changed to phpunit

---
+++ laravel/tests/Unit/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Auth\Events\Lockout;
+
+class ExampleTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
